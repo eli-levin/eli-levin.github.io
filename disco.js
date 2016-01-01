8eff8470ae19de7bed19200c648b5ae245b65932
Eli Levin <eli.j.wasserman@gmail.com>
2016-01-01 14:10:03 -0600

    clean up the shapes instead of disco tiles

--- disco.js
+++ disco.js
@@ -1,13 +1,9 @@
-//right now will be blue waves
-//eventually change to rainbow
 var w = window.innerWidth;
 var h = window.innerHeight;
 //var num = w * h / 810;
 
 var fromLeft = 0;
 var fromTop = 0;
-var rowss = 9;
-var colss = 9;
 
 function makeTiles(rows, cols) {
     for (x = 0; x < rows; x++) {
@@ -16,17 +12,23 @@ function makeTiles(rows, cols) {
             //var i = 1/(x*cols+y) * 8888; //better colors...
             var i = Math.random() * 210 + 250;
             //var randColor = '#'+Math.floor(Math.random()*16777215).toString(16); //this was old col
+            fromLeft = Math.random() * 95;
+            fromTop = Math.random() * 95;
+            
             var col = 'hsla(' + i + ',90%, 60%, 1)';
             var tile = document.createElement('BUTTON');
             tile.className = "tile";
-            tile.style.left = fromLeft + "px";
-            tile.style.top = fromTop + "px";
+            tile.style.left = fromLeft + "%";
+            tile.style.top = fromTop + "%";
             tile.style.backgroundColor = col;
+            //rotate the tiles randomly-- the prime # 17 is good for this
+            var rotate = 360*Math.random();
+            tile.style.mozTransform = 'rotate('+(rotate-17)+'deg)';
+            tile.style.webkitTransform = 'rotate('+(rotate-17)+'deg)';
+            tile.style.transform = 'rotate('+(rotate-17)+'deg)';
+            //add the tile to the html
             document.getElementById('board').appendChild(tile);
-            fromLeft += 55;
         }
-        fromTop += 55;
-        fromLeft = 0;
     }
 }
 
@@ -41,9 +43,13 @@ function makeTiles(rows, cols) {
 
 
 $(document).ready(function () {
+    //set glboal w and h to width and height of board
+    w = $('#board').width();
+    h = $('#board').height();
+  
     //make the disco tiles
-    var rows = $('#board').width() / 60;
-    var cols = $('#board').height() / 60;
+    var rows = w / 30;
+    var cols = h / 30;
     makeTiles(rows, cols);
 
     $('button').mouseenter(function () {
@@ -57,27 +63,15 @@ $(document).ready(function () {
         var ind = $(this).index();
         ind++;
         var myBut = "button:nth-child(" + ind + ")";
-        $(myBut).fadeTo(100, 0.5);
+        //$(myBut).fadeTo(100, 0.5);
+        $(this).hide();
     });
 
-    $('button').click(function () {
+    /*$('button').click(function () {
         var ind = $(this).index();
         ind++;
         var myBut = "button:nth-child(" + ind + ")";
-        ripple(ind);
-    });
-
-    $('button').on("taphold", function () {
-        var ind = $(this).index();
-        ind++;
-        var myBut = "button:nth-child(" + ind + ")";
-        $(myBut).fadeTo(100, 1);
-    });
-
-    $('button').on("taphold", function () {
-        var ind = $(this).index();
-        ind++;
-        var myBut = "button:nth-child(" + ind + ")";
-        $(myBut).fadeTo(100, 0.5);
-    });
+        //ripple(ind);
+        //$(this).hide();
+    });*/
 });
